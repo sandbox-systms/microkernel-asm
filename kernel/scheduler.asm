d388e310ceda5f99478cfc8b2d7fcde4ccb641de
sandbox <ghostnether28@gmail.com>
2026-03-15 01:53:30 -0300

    kernel - add implement round robin task switching

---
+++ kernel/scheduler.asm
@@ -0,0 +1,23 @@
+; =========================================
+; Definição de qual tarefa será executada:
+; Modelo simple round-robin 
+; =========================================
+
+init_scheduler: 
+
+    mov byte [current_task], 0
+    ret 
+
+schedule:
+
+    inc byte [current_task]
+    cmp byte [current_task], MAX_TASKS 
+    jl done 
+
+    mov byte [current_tasks], 0
+
+done:
+    ret 
+
+    MAX_TASKS equ 4
+    current_tasks db 0 
